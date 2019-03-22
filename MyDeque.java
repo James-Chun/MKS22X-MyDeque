97b90e9e2b522e4404cc1b0ed86f0de7da84f536
James Chun <jchun00@stuy.edu>
2019-03-22 09:48:50 -0400

    completed some of the simple methods - ones that return a value from list of remove (adding is still a bit confusing)

--- MyDeque.java
+++ MyDeque.java
@@ -1,5 +1,4 @@
 
-
 public class MyDeque<E>{
     private E[] data;
     private int size, start, end;
@@ -7,25 +6,60 @@ public class MyDeque<E>{
     @SuppressWarnings("unchecked")
     public MyDeque(){
         data = (E[])new Object[10];
+        start = 0;
+        end = 9;
+        size = 10;
     }
 
     @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
         data = (E[])new Object[initialCapacity];
+        start = 0;
+        end = initialCapacity - 1;
+        size = initialCapacity;
     }
+
+
     public int size(){
-      return data.length;
+      return size;
     }
+
+
     public String toString(){
         String visual = "{";
-        for (int i=0; i<data.length; i++){
+        for (int i=start; i<=end; i++){
           visual = visual + data[i] + " ";
         }
         return visual + "}";
     }
 
+    //public void addFirst(E element){ }
+    //public void addLast(E element){ }
+    public E removeFirst(){
+        E removed = data[start];
+        data[start]=null;
+        start++;
+        return removed;
+    }
+    public E removeLast(){
+        E removed = data[end];
+        data[end]=null;
+        end--;
+        return removed;
+    }
+    public E getFirst(){
+        return data[start];
+    }
+    public E getLast(){
+        return data[end];
+    }
+
+
     public static void main(String[] args){
       MyDeque<String> test = new MyDeque<String>();
+
+      test.removeFirst();
+      test.removeLast();
       System.out.println(test);
     }
 }
