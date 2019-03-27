947f4a71b7ce0a519ef0086a9b46bb3391336fd9
James Chun <jchun00@stuy.edu>
2019-03-27 13:05:49 -0400

    got general idea down

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      MyDeque<Integer> instructions = new MyDeque<>();
+      for (int i=0;i<s.length();i++){
+          if ()   //need way to check for spaces (probably some parse or whatever Scanner thing)
+                  //if int add to deque if not take last two ints and do operations and then remove add accordingly
+                  
+      }
+    }
+}
