296d3bfadfbc3c372fc412d195e4c0300c5da14d
James-Chun <jchun00@stuy.edu>
2019-03-27 22:48:35 -0400

    got general algorithim down but something is wrong

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,55 @@
+import java.util.*;
+
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-     private static String[] stuff;
+    //private static String[] stuff;
     public static double eval(String s){
-      MyDeque<Integer> instructions = new MyDeque<>();
 
-      stuff = s.split(" ");
-      for (int i=0;i<s.length();i++){
-          //if ()   //need way to check for spaces (probably some parse or whatever Scanner thing)
-                  //if int add to deque if not take last two ints and do operations and then remove add accordingly
+      MyDeque<Double> valuesBeingEvaluated = new MyDeque<>();
+
+      String[] valuesToBeEvaluated = s.split(" ");
+
+      double temp = 0.0;
+      for (int i=0;i<valuesToBeEvaluated.length;i++){//if int add to deque if not take last two ints and do operations and then remove add accordingly
+
+          try{
+            valuesBeingEvaluated.addLast( Double.parseDouble( valuesToBeEvaluated[i]) );
+          }catch(Exception NumberFormatException){}
+
+
+          if (valuesToBeEvaluated[i] == "+"){
+              temp = valuesBeingEvaluated.removeFirst() + valuesBeingEvaluated.removeFirst();
+              valuesBeingEvaluated.addLast( temp );
+
+                        System.out.println(valuesToBeEvaluated[i]);
+          }
+          else if (valuesToBeEvaluated[i] == "-"){
+              valuesBeingEvaluated.addLast( valuesBeingEvaluated.removeFirst() - valuesBeingEvaluated.removeFirst() );
+
+                        System.out.println(valuesToBeEvaluated[i]);
+          }
+          else if (valuesToBeEvaluated[i] == "*"){
+              valuesBeingEvaluated.addLast( valuesBeingEvaluated.removeFirst() * valuesBeingEvaluated.removeFirst() );
+                        System.out.println(valuesToBeEvaluated[i]);
+          }
+          else if (valuesToBeEvaluated[i] == "/"){
+            temp = valuesBeingEvaluated.removeFirst() / valuesBeingEvaluated.removeFirst();
+            valuesBeingEvaluated.addLast( temp );
+                      System.out.println(valuesToBeEvaluated[i]);
+          }
+          else if (valuesToBeEvaluated[i] == "%"){
+              valuesBeingEvaluated.addLast( valuesBeingEvaluated.removeFirst() % valuesBeingEvaluated.removeFirst() );
+                        System.out.println(valuesToBeEvaluated[i]);
+          }
+
 
       }
-      return 0.0;
+      return valuesBeingEvaluated.getLast();
     }
 
-
+/*
     public static String visual(){
       String visual = "";
       for (int i=0;i<stuff.length;i++){
@@ -26,10 +60,20 @@ public class Calculator{
       }
       return visual;
     }
+*/
 
     public static void main(String[] args){
-      eval("10 2 3 4 5 + * - -");
-      System.out.println(visual());
+      //eval("10 2 3 4 5 + * - -");
+      //System.out.println(visual());
+      System.out.println( eval("1 2 +") );
+      //System.out.println( "" + 1.0 + 2.0 );
+
+      /*MyDeque<Double> v = new MyDeque<>();
+      v.addLast(1.0);
+      v.addLast(2.0);
+      double temp = v.removeFirst() + v.removeFirst();
+      v.addLast(temp);
+      System.out.println(v.getFirst());*/
     }
 
 }
